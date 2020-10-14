35605be9272e915ff8276c7c1cb220e44b2b3f1c
Kwounsu <sb00lgs@gmail.com>
2020-10-14 07:34:28 -0700

    Cannot inline bytecode built with JVM target 1.8 into bytecode that is being built with JVM target 1.6. Please specify proper '-jvm-target' option

--- app/src/main/java/com/example/mvprxjava/MainActivity.kt
+++ app/src/main/java/com/example/mvprxjava/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.mvprxjava
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import io.reactivex.subjects.BehaviorSubject
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.editText)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        val subject = BehaviorSubject.createDefault("0")
+        editText.addTextChangedListener {
+
+        }
     }
 }
